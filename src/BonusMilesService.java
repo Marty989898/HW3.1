9bdcacbb7a063d5965ebff43342da256cb926640
Maksim <Maks.martinov98@yandex.ru>
2023-04-11 18:23:47 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int bonus = (20);
+        int miles = (price / bonus);
+        System.out.println("Ваш Бонус:");
+        return miles;
+    }
+
+}
